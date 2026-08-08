1e7ab19a663bb13d43c017e808acee16922ebe73
yavarmish <59411957+yavarmish@users.noreply.github.com>
2026-08-08 16:23:05 +0530

    Add: network-delay-time - submission-3

---
+++ Data Structures & Algorithms/network-delay-time/submission-3.cpp
@@ -0,0 +1,36 @@
+#define MAX_DIST 1e8
+class Solution {
+    unordered_map<int, vector<pair<int, int>>> adj;
+public:
+    // SPFA(using queue instead): Time -> O(V*E)[Worst for dense graphs] O(V+E)[Average] Space -> O(V + E)
+    // Dijkstra: Time -> O(ElogV) Space -> O(V + E)
+    void getLastTime(int src, vector<int> &res) {
+        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> bfsQueue;
+        bfsQueue.push({0, src});
+        while(bfsQueue.size()) {
+            auto [time, node] = bfsQueue.top();
+            bfsQueue.pop();
+            if(time > res[node]) continue;
+            for(auto &[nbrNode, delay] : adj[node]) {
+                int curTime = time + delay;
+                if(curTime < res[nbrNode]) {
+                    res[nbrNode] = curTime;
+                    bfsQueue.push({curTime, nbrNode});
+                }
+            }
+        }
+    }
+    int networkDelayTime(vector<vector<int>>& times, int n, int k) {
+        for(auto &time : times) {
+            adj[time[0]].push_back({time[1], time[2]});
+        }
+        vector<int> res(n + 1, MAX_DIST);
+        res[k]= 0;
+        getLastTime(k, res);
+        int ans = 0;
+        for(int i = 1; i <= n; ++i) {
+            ans = max(ans, res[i]);
+        }
+        return ans == MAX_DIST ? -1 : ans;
+    }
+};
