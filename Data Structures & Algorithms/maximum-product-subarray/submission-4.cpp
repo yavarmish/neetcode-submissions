7de23416d9955f5ade94c105e276bb5b066258d7
yavarmish <59411957+yavarmish@users.noreply.github.com>
2026-08-14 17:47:34 +0530

    Add: maximum-product-subarray - submission-4

---
+++ Data Structures & Algorithms/maximum-product-subarray/submission-4.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int maxProduct(vector<int>& nums) {
+        // Mantain CurMax and CurMin
+        int curMax = 1, curMin = 1, res = INT_MIN;
+        for(auto &num : nums) {
+            /* 
+            -ve num swaps inequality 
+            eg. curMax = 8 curMin = 2 num = -3
+            swapped -> curMax = 2 * -3 and curMin = 8 * -3
+            */
+            if(num < 0) swap(curMax, curMin);
+            // update and reset if required
+            curMax = max(num, curMax * num);
+            curMin = min(num, curMin * num);
+            res = max(curMax, res);
+        }
+        return res;
+    }
+};
